5303c0a0a9acfa88f0e85ca4602d5d448c2e2905
jchen <jich@umich.edu>
2014-01-16 15:13:20 -0500

    update calculate.cube.id.pl. add running time to command line.

--- script/calculate.cube.id.pl
+++ script/calculate.cube.id.pl
@@ -56,6 +56,16 @@ if (!defined($logFile))
 	exit(1);
 }
 
+my ($sec,$min,$hour,$day,$mon,$year,$weekday,$yeardate,$savinglightday) = (localtime(time));
+$sec = ($sec < 10)? "0$sec":$sec;
+$min = ($min < 10)? "0$min":$min;
+$hour = ($hour < 10)? "0$hour":$hour;
+$day = ($day < 10)? "0$day":$day;
+$mon = ($mon < 9)? "0".($mon+1):($mon+1);
+$year += 1900;
+
+my $now = "$year-$mon-$day $hour:$min:$sec";
+
 my $logFileLock = $logFile.".lck";
 
 my %mafHash;
@@ -197,7 +207,7 @@ flock(SEM,LOCK_EX) or die "Lock failed: $!";
 
 open (OUT,">>".$logFile) || die "can't write to the file:$!\n";
 
-print OUT "perl calculate.cube.id.pl --chrDIR $chrDIR --out $out --logFile $logFile start=$start end=$end runningTime=$runningTime\n";
+print OUT "$now perl calculate.cube.id.pl --chrDIR $chrDIR --out $out --logFile $logFile start=$start end=$end runningTime=$runningTime\n";
 
 close OUT;
 
